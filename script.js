d0512f4f19e428ff0f0ea31e35221a0d1be0d19e
HumayunSaghir <hr3296726@gmail.com>
2025-09-06 02:26:14 +0500

    Functionality Under Development

---
+++ script.js
@@ -0,0 +1,27 @@
+// Target your elements
+let resultField = document.querySelector('.resultField')
+let ac = document.querySelector('.ac')
+let del = document.querySelector('.del')
+let percentage = document.querySelector('.percentage')
+let slash = document.querySelector('.slash')
+let seven = document.querySelector('.seven')
+let eight = document.querySelector('.eight')
+let nine = document.querySelector('.nine')
+let multiply = document.querySelector('.multiply')
+let four = document.querySelector('.four')
+let five = document.querySelector('.five')
+let six = document.querySelector('.six')
+let minus = document.querySelector('.minus')
+let one = document.querySelector('.one')
+let two = document.querySelector('.two')
+let three = document.querySelector('.three')
+let plus = document.querySelector('.plus')
+let zero = document.querySelector('.zero')
+let dot = document.querySelector('.dot')
+let equal = document.querySelector('.equal')
+
+// console.table([resultField, ac, del, percentage, slash,
+//     seven, eight, nine, multiply, four, five, six, minus,
+//     one, two, three, plus, zero, dot, equal
+// ])
+
